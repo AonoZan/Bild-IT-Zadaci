f631f187621be76a1c669ba219cc723eab6091a7
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-01 20:12:40 +0200

    Last one...

---
+++ Bild-IT-Zadaci/zadaci_01_08_2016/Zadatak_05.java
@@ -0,0 +1,145 @@
+/**@autor AonoZan Dejan Petrovic 2016 ©
+ */
+package zadaci_01_08_2016;
+
+public class Zadatak_05 {
+	// Create variable that holds Scanner and method that closes it
+	private static java.util.Scanner userInput = new java.util.Scanner(System.in);
+	private static void closeUserInput() {userInput.close();}
+	/**
+	 * Method for cleaning list from specific value. 
+	 * @param list is what will be cleaned
+	 * @param regex every value that match this in list will be removed
+	 * @return
+	 */
+	public static int[] cleanList(int[] list, int regex) {
+		// create default values
+		StringBuilder cleanedList = new StringBuilder();
+		int length = 0;
+		// go trough every item in list and append it to sting if its not regex
+		for (int i : list) {
+			if (i != regex) {
+				cleanedList.append(i + " ");
+				length++;
+			}
+		}
+		// make list size match length of how much items are counted
+		list = new int[length];
+		// go trough splited list, convert it to int and add it to list
+		for (String s : cleanedList.toString().split(" ")) {
+			list[list.length-length] = Integer.valueOf(s);
+			length--;
+		}
+		return list;
+	}
+	/**
+	 * Method that reads only int values from console.
+	 * If something else is entered an exception is raised.
+	 * @param number this is needed to tell Java that we need int value from user
+	 * @return int value
+	 * @throws Exception This is raised if user enters string or character
+	 */
+	public static int readFromConsole(int number) throws Exception{
+		// gather input
+		String consoleInput = userInput.nextLine().trim();
+		// if only whitespaces are entered raise exception
+		if (consoleInput.length() < 1) throw new Exception("Empty input.(whitespace)");
+		// try to parse int from first word (splited string)
+		try {
+			number = Integer.parseInt(consoleInput.split(" ")[0]);
+		// if error occurs throw specific exception to tell what happened
+		} catch (Exception e) {
+			throw new Exception("Input is not a number.");
+		}
+		return number;
+	}
+	/**
+	 * Method to make easy entering lots of integer values from console.
+	 * List will be updated and new values will be added infinetely until 0 value is entered.
+	 * @param list to be filled with walues
+	 * @return list that has user entered values
+	 */
+	public static int[] fillListFromConsoleInput(int[] list) {
+		// set default values
+		int inputNumber = 1, counter = 0;
+		// loop until 0 is entered
+		while(inputNumber != 0) {
+			// try to get right value from user
+			try {
+				System.out.print("Enter number(0 to stop): ");
+				inputNumber = readFromConsole(inputNumber);
+				list[counter] = inputNumber;
+				counter++;
+				// if list gets filled double the space
+				if (counter == list.length) {
+					int[] userNumbersTmp = new int[counter * 2];
+					for (int i = 0; i < counter; i++) {
+						userNumbersTmp[i] = list[i];
+					}
+					list = userNumbersTmp;
+				}
+				System.out.println();
+			// if error occurs print message
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		// return cleaned list from 0
+		return cleanList(list, 0);
+	}
+	/**
+	 * Method for printing status message of occurence of largest number in list.
+	 * If list contains one element it will be handled immediately as well if list is null.
+	 * @param list of integers
+	 */
+	public static void printLargest(int[] list) {
+		int[] sortedList;
+		String fixWord = "";
+		try {
+			// if list contains only one element there is no need to go further
+			if (list.length == 1) {
+				System.out.printf("Only one element in list: %d\n", list[0]);
+				return;
+			}
+			// try to sort list
+			sortedList = Zadatak_04.sort(list);
+//			// print list for debug purpose
+//			Zadatak_04.printList(sortedList);
+		// if error occurs print message and return
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return;
+		}
+		// go from next-to last element to first one
+		for (int i = sortedList.length-2; i >= 0; i--) {
+			// if you hit diferent value or you reach end of the list
+			if (sortedList[i] != sortedList[i+1] || i == 0) {
+				// set appropriate fixWord
+				if (sortedList.length - i != 0) fixWord = "'s";
+				// print status and break 
+				System.out.printf("Largest number taken from console is %d and it occured %d time%s.",
+						sortedList[i+1], sortedList.length - i - Math.min(sortedList[i+1] - sortedList[i], 1), fixWord);
+				break;
+			}
+		}
+	}
+	/**
+	 * Main method that creates list and fills it with values from console until 0 is inserted.
+	 * And then prints largest number in list as well as how many times that number occured.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// create list and fill it with values that user provides
+		int[] userNumbers = fillListFromConsoleInput(new int[1]);
+//		// try to print list
+//		try {
+//			Zadatak_04.printList(userNumbers);
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+		// print status of largest in list and how much of those in list
+		printLargest(userNumbers);
+		// close Scanner
+		closeUserInput();
+	}
+}
