c16a58e22d5b76ff6e3d0bea17b6b01981960583
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-05 14:18:19 +0200

    Added comments.

--- Bild-IT-Zadaci/zadaci_05_08_2016/Zadatak_03.java
+++ Bild-IT-Zadaci/zadaci_05_08_2016/Zadatak_03.java
@@ -3,6 +3,12 @@
 package zadaci_05_08_2016;
 
 public class Zadatak_03 {
+	/**
+	 * Method for calculating prefix of two strings.
+	 * @param first Can be any value of String type
+	 * @param second Can be any value of String type
+	 * @return Calculated prefix as String or null if there is no one or any of the argument is null or empty
+	 */
 	public static String prefix(String first, String second) {
 		// if any of argument is null return null
 		if (first == null || first.length() == 0) {
@@ -19,14 +25,18 @@ public class Zadatak_03 {
 		}
 		return prefix.toString();
 	}
+	/**
+	 * Program asks user to enter two strings and then calculates and prints prefix tor those.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-//		System.out.println("asdasdef".equals("asdasdf"));
+		// prompt usr to enter two strings
 		System.out.println("Enter two strings.");
 		System.out.print("Enter first string: ");
 		String firstString = zadaci_01_08_2016.Zadatak_02.userInput.nextLine();
 		System.out.print("Enter second string: ");
 		String secondString = zadaci_01_08_2016.Zadatak_02.userInput.nextLine();
-		
+		// check if any prefix and print if any
 		String prefix = prefix(firstString, secondString);
 		System.out.printf("%s\n", prefix.length() > 0 ? "Prefix for both strings is: \"" + prefix + "\"" : "No prefix.");
 		zadaci_01_08_2016.Zadatak_02.closeUserInput();
