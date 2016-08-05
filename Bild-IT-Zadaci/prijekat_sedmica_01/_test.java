d6a728d86bcaf0cd152599991a2712b9d99f44a6
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-05 08:39:34 +0200

    Begin work on mini project.

---
+++ Bild-IT-Zadaci/prijekat_sedmica_01/_test.java
@@ -0,0 +1,10 @@
+/**@autor AonoZan Dejan Petrovic 2016 ©
+ */
+package prijekat_sedmica_01;
+
+public class _test {
+	public static void main(String[] args) {
+		MonthPresentation month = new MonthPresentation();
+		
+	}
+}
