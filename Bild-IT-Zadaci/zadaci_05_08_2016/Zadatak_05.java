20a5a5e5d485222ebec78bd20cc49c22f9ddc216
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-05 13:20:04 +0200

    First upload...

---
+++ Bild-IT-Zadaci/zadaci_05_08_2016/Zadatak_05.java
@@ -0,0 +1,12 @@
+/**@autor AonoZan Dejan Petrovic 2016 ©
+ */
+package zadaci_05_08_2016;
+
+public class Zadatak_05 {
+	public static int countLetters(String s) {
+		return 0;
+	}
+	public static void main(String[] args) {
+		zadaci_01_08_2016.Zadatak_02.closeUserInput();
+	}
+}
