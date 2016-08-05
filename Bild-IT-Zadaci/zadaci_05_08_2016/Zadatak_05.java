500b66717af4c9b3b9c3c726f2733413dabbd95b
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-05 13:28:56 +0200

    countLetters() finished

--- Bild-IT-Zadaci/zadaci_05_08_2016/Zadatak_05.java
+++ Bild-IT-Zadaci/zadaci_05_08_2016/Zadatak_05.java
@@ -3,10 +3,26 @@
 package zadaci_05_08_2016;
 
 public class Zadatak_05 {
+	/**
+	 * Method counts how many letters string containd
+	 * @param s String of any length.
+	 * @return Number of letters in string or 0 if null is passed as argument
+	 */
 	public static int countLetters(String s) {
-		return 0;
+		// return 0 if null is passed
+		if (s == null) {
+			System.out.println("Can't count letters(null as argument).");
+			return 0;
+		}
+		// go trough whole string and check how much letters it contain
+		int counter = 0;
+		for (char ch : s.toCharArray()) {
+			if (ch > 'a' && ch < 'z' || ch > 'A' && ch < 'Z') counter++;
+		}
+		return counter;
 	}
 	public static void main(String[] args) {
+		
 		zadaci_01_08_2016.Zadatak_02.closeUserInput();
 	}
 }
