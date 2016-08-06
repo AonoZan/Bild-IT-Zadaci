8d33d15ad8307d3ae80be426be202317dbfea87a
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-06 14:38:58 +0200

    First upload...

---
+++ Bild-IT-Zadaci/zadaci_06_08_2016/Zadatak_03.java
@@ -0,0 +1,16 @@
+/**@autor AonoZan Dejan Petrovic 2016 ©
+ */
+package zadaci_06_08_2016;
+
+public class Zadatak_03 {
+	public static int reverse(int number) {
+		return number;
+	}
+	public static boolean isPAlindrome(int number) {
+		return true;
+	}
+	public static void main(String[] args) {
+
+		zadaci_01_08_2016.Zadatak_02.closeUserInput();
+	}
+}
