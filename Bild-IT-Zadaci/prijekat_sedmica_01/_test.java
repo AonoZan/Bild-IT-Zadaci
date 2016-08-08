232f160e2278841e69839ce4014100c60359bf32
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-08 20:40:19 +0200

    Added comments.

--- Bild-IT-Zadaci/prijekat_sedmica_01/_test.java
+++ Bild-IT-Zadaci/prijekat_sedmica_01/_test.java
@@ -3,12 +3,16 @@
 package prijekat_sedmica_01;
 
 public class _test {
+	/**
+	 * Louncher for application.
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// create app and run it while is acsive
 		App aplication = new App();
 		while(aplication.isActive()) {
 			aplication.run();
 		}
 		aplication.exit();
-		
 	}
 }
