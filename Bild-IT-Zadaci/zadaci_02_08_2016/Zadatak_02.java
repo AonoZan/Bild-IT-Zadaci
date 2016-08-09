b53e8b4e6d9e61337d527c2e1658ff8e9bd173c0
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-09 16:50:47 +0200

    Made method return boolean value instead.

--- Bild-IT-Zadaci/zadaci_02_08_2016/Zadatak_02.java
+++ Bild-IT-Zadaci/zadaci_02_08_2016/Zadatak_02.java
@@ -30,79 +30,90 @@ public class Zadatak_02 {
 	}
 	/**
 	 * Method for asking simple mathematical questions. Several opearations can be calculated.
-	 * @param questionModifiers must be of length 3. First value tells how many questions will be asked,
-	 * second and third are range for random number
+	 * @param questionModifiers contains range in which random numbers should be generated
 	 * @param operation holds any number of srting values(operations) between numbers, can be any number of "+", "-"
+	 * @param forceCorrect if true program will ask same question untill corect answer is entered
 	 */
-	public static void askSimpleQuestion(int[] questionModifiers, String[] operation) {
+	public static boolean askSimpleQuestion(int[] questionModifiers, String[] operation, boolean forceCorrect) {
 		// make sure that at least one question and operation is asked and used 
 		if (questionModifiers == null || questionModifiers.length != 3) questionModifiers = new int[]{1, 1, 9};
 		if (operation == null || operation.length == 0) operation = new String[]{"+"};
 		// set default values
-		int correctAnswers = 0, incorrectAnswers = 0;
 		int result = 0, userAnswer = 0, randomNumber;
-		// loop for number of questions from first value in questionMdifiers
-		for (int i = 0; i < questionModifiers[0]; i++) {
-			// generate new random number and add to string and result
+		// generate new random number and add to string and result
+		randomNumber = genRandInt(questionModifiers[1], questionModifiers[2]);
+		result = randomNumber;
+		String questionString = randomNumber + " ";
+		// loop for any number of operation there is
+		for (String string : operation) {
+			// generate every other number based on how many is needed
 			randomNumber = genRandInt(questionModifiers[1], questionModifiers[2]);
-			result = randomNumber;
-			String questionString = randomNumber + " ";
-			// loop for any number of operation there is
-			for (String string : operation) {
-				// generate every other number based on how many is needed
-				randomNumber = genRandInt(questionModifiers[1], questionModifiers[2]);
-				// depending on what current operation is calculate result and update string
-				switch (string) {
-					case "+":
-						result += randomNumber;
-						questionString += "+ " + randomNumber + " ";
-						break;
-					case "-":
-						result -= randomNumber;
-						questionString += "- " + randomNumber + " ";
-						break;
-					default:
-						break;
-				}
+			// depending on what current operation is calculate result and update string
+			switch (string) {
+				case "+":
+					result += randomNumber;
+					questionString += "+ " + randomNumber + " ";
+					break;
+				case "-":
+					result -= randomNumber;
+					questionString += "- " + randomNumber + " ";
+					break;
+				default:
+					break;
 			}
-			// at the end add equal sighn
-			System.out.print(questionString + "= ");
 			// try to get user answer
 			while (true) {
 				try {
+					// at the end add equal sign
+					System.out.print(questionString + "= ");
 					userAnswer = Zadatak_05.readFromConsole(userAnswer);
+					if (forceCorrect && result != userAnswer)
+						throw new Exception("That's not correct answer.");
 					break;
 				} catch (Exception e) {
-					System.out.println(e.getMessage() + "\nTry again to answer question: ");
+					System.out.println(e.getMessage() + "\nTry again to answer question. ");
 					
 				}
 			}
-			// update answer counters
-			if (result == userAnswer) correctAnswers++;
-			else incorrectAnswers++;
 		}
-		// print congration message if all correct or status if few are incorrect
-		if (incorrectAnswers == 0) System.out.println("You answered all correctly!");
-		else System.out.printf("You answered %d correct and %d incorrect.", correctAnswers, incorrectAnswers);
-		
-		// 1 + 2 =
-		// 1 |+ 2 |=
-		// 1 |+ 2 |+ 2 |=
+		if (result == userAnswer) return true;
+		return false;
 	}
+	/**
+	 * Program asks user for one number and using that number asks that many simple mathematical questions.
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception{
-		int numberOfQuestions = 0;
+		// create variables and try to get values from user
+		int numberOfQuestions = 0, correctAnswers = 0;
 		while (true) {
 			try {
-				System.out.print("Enter number of questions: ");
-				numberOfQuestions = Zadatak_05.readFromConsole(numberOfQuestions);
+				// if variable for questions is still on default get new one
+				if (numberOfQuestions == 0) {
+					System.out.print("Enter number of questions: ");
+					numberOfQuestions = Zadatak_05.readFromConsole(numberOfQuestions);
+					// if value is still negative or zero reset it and ask for another one
+					if (numberOfQuestions < 1) {
+						System.out.printf("%d is not correct value.\n", numberOfQuestions);
+						numberOfQuestions = 0;
+						throw new Exception("Try again...");
+					}
+				}
 				break;
 			} catch (Exception e) {
-				System.out.println(e.getMessage() + "\nTry again...");
+				System.out.println(e.getMessage());
 				
 			}
 		}
-//		System.out.println(numberOfQuestions);
-		askSimpleQuestion(new int[]{numberOfQuestions, 1, 9}, new String[]{"-"});
+		// loop for number of questions and count correct ones
+		for (int i = 0; i < numberOfQuestions; i++) {
+			if (askSimpleQuestion(new int[]{1, 9}, new String[]{"-"}, true)) 
+				correctAnswers++;
+		}
+		// print congration message if all correct or status if few are incorrect
+		if (correctAnswers == numberOfQuestions) System.out.println("You answered all correctly!");
+		else System.out.printf("You answered %d correct and %d incorrect.", correctAnswers, numberOfQuestions - correctAnswers);
 		zadaci_01_08_2016.Zadatak_02.closeUserInput();
 	}
 }
