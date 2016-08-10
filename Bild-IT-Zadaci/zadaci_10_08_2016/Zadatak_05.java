0d3f4a3fe1f8dd7f9929cdf649281a1ef887994d
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-10 21:57:29 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_10_08_2016/Zadatak_05.java
+++ Bild-IT-Zadaci/zadaci_10_08_2016/Zadatak_05.java
@@ -3,16 +3,31 @@
 package zadaci_10_08_2016;
 
 public class Zadatak_05 {
+	/**
+	 * Method checks if string contains substring from arguments.
+	 * @param string
+	 * @param substring
+	 * @return
+	 */
 	public static boolean isSubstring(String string, String substring) {
-		for (int i = 0; i < string.length() - substring.length(); i++) {
-			if (substring.equals(string.substring(i, i + substring.length()))) {
+		if (string == null || substring == null) {										// if arguments are null return false
+			System.out.println("Can't check if string has substring(null argument).");
+			return false;
+		} else if (string.length() < substring.length()) return false;					// if string is smaller also false
+		else if (string.length() == substring.length()) return string.equals(substring);// if equal length then return if they are equal
+		for (int i = 0; i < string.length() - substring.length(); i++) {				// loop trough string
+			if (substring.equals(string.substring(i, i + substring.length()))) {		// and check if there is match to substring
 				return true;
 			}
 		}
 		return false;
 	}
-	
+	/**
+	 * Program tests method isSubstring().
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		System.out.print("\"BC\" is substring of \"ABCD\": ");
 		System.out.println(isSubstring("ABCD", "BC"));
 	}
 }
