3bba41e1b9bd31383b1b535bf0b668f122dfe3b2
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-10 18:59:24 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_10_08_2016/Zadatak_02.java
+++ Bild-IT-Zadaci/zadaci_10_08_2016/Zadatak_02.java
@@ -12,6 +12,16 @@ public class Zadatak_02 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		// loop for all numbers and print only ones that are twin primes
+		System.out.println("All twin primes less than 10000:");
+		for (int i = 2, counter = 0; i < 10000; i++) {
+			if (zadaci_03_08_2016.Zadatak_01.isPrime(i) && zadaci_03_08_2016.Zadatak_01.isPrime(i + 2)) {
+				// print left and right prime justified correctly
+				System.out.printf("%4d|%-4d ", i, (i + 2));
+				// count and print new line after 10 twins
+				counter++;
+				if (counter % 10 == 0) System.out.println(" " + counter / 10);
+			}
+		}
 	}
 }
