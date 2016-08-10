df48274b20ffbc201eee35d065de5ce1044e671e
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-10 11:33:49 +0200

    First upload.

---
+++ Bild-IT-Zadaci/zadaci_09_08_2016/Zadatak_04.java
@@ -0,0 +1,16 @@
+/**@autor AonoZan Dejan Petrovic 2016 ©
+ */
+package zadaci_09_08_2016;
+
+public class Zadatak_04 {
+	/**
+	 * Program reads unlimited anount of numbers from user.
+	 * Reading stops ehen 0 is entered.
+	 * After 0 is entered program prints which number is largest and how many times it occured.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		zadaci_01_08_2016.Zadatak_02.closeUserInput();
+	}
+}
