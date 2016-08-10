d321878931af90952a54f5e4fbd76cfc5cfa01ee
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-10 17:45:37 +0200

    First upload.

---
+++ Bild-IT-Zadaci/zadaci_10_08_2016/Zadatak_02.java
@@ -0,0 +1,17 @@
+/**@autor AonoZan Dejan Petrovic 2016 ©
+ */
+package zadaci_10_08_2016;
+
+public class Zadatak_02 {
+	/**
+	 * Program prints all primes twins that are less than 10000, 10 pairs on each line.
+	 * <p><b>Example of twin primes:</b>
+	 * <br>3 and 5
+	 * <br>5 and 7
+	 * <br>11 and 13
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
