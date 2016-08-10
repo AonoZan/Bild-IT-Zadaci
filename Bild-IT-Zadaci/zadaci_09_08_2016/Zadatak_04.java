95ef89ca7e66d4d20d9407efe73415fd340f913b
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-10 12:29:42 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_09_08_2016/Zadatak_04.java
+++ Bild-IT-Zadaci/zadaci_09_08_2016/Zadatak_04.java
@@ -12,32 +12,34 @@ public class Zadatak_04 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// createt scanner but use delimeter that include only space
 		Scanner input = new Scanner(System.in);
 		input.useDelimiter(" *");
+		// prompt user for numbers and count them until 0 is entered
 		System.out.println("Enter unlimited amount of numbers.");
 		int userNumber = -1, largestNumber = 0, counter = 0;
 		while (userNumber != 0) {
-			try {
-				System.out.printf("Enter %snumbers: ", counter > 0 ? "some more " : "");
-				while (input.hasNextInt()) {
-					userNumber = input.nextInt();
-					if (userNumber != 0) {
-						if (userNumber > largestNumber) {
-							largestNumber = userNumber;
-							counter = 1;
-						} else if (userNumber == largestNumber)
-							counter++;
-						continue;
-					}  else {
-						break;
-					}
+			// ask for numbers while there is some in scanner
+			System.out.printf("Enter %snumbers: ", counter > 0 ? "some more " : "");
+			while (input.hasNextInt()) {
+				userNumber = input.nextInt();
+				// if number is other than 0 else break
+				if (userNumber != 0) {
+					// if number is largest set largest
+					if (userNumber > largestNumber) {
+						largestNumber = userNumber;
+						counter = 1;
+					// else if its same as largest count it
+					} else if (userNumber == largestNumber)
+						counter++;
+				}  else {
+					break;
 				}
-				input.nextLine();
-			} catch (Exception e) {
-				System.out.println("Illegal input.");
-				System.exit(0);
 			}
+			// clear input for next loop
+			input.nextLine();
 		}
+		// print status
 		System.out.printf("Largest number was %d and occured %d time%s.",
 				largestNumber,
 				counter,
