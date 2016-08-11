866a5a2fe83573cc50fb29751d7cf6368439158d
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-11 10:39:15 +0200

    First upload.

---
+++ Bild-IT-Zadaci/zadaci_11_08_2016/Zadatak_01.java
@@ -0,0 +1,10 @@
+/**@autor AonoZan Dejan Petrovic 2016 ©
+ */
+package zadaci_11_08_2016;
+
+public class Zadatak_01 {
+	public static void main(String[] args) {
+
+		zadaci_01_08_2016.Zadatak_02.closeUserInput();
+	}
+}
