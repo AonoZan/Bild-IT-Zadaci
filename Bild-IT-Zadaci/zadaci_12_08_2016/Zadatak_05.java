98ced5f4059be9eb62c405c7adb020edbea7df95
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-12 15:50:45 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_12_08_2016/Zadatak_05.java
+++ Bild-IT-Zadaci/zadaci_12_08_2016/Zadatak_05.java
@@ -1,7 +1,6 @@
 /**@autor AonoZan Dejan Petrovic 2016 ©
  */
 package zadaci_12_08_2016;
-
 import java.util.Scanner;
 
 public class Zadatak_05 {
@@ -11,13 +10,25 @@ public class Zadatak_05 {
 	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		// create list for counting numbers and user number
 		int[] numbers = new int[100];
 		int userNumber = -1;
+		// prompt user to enter numbers and take until 0 is entered
 		System.out.println("Enter numbers: ");
 		while (userNumber != 0) {
 			userNumber = input.nextInt();
 			if (userNumber > 0) {
+				// count number as list index
 				numbers[userNumber - 1]++;
+			}
+		}
+		// loop over list and print those indexes that are greater than 0
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] > 0) {
+				System.out.printf("Number %d occured %d time%s.\n",
+						i + 1,
+						numbers[i],
+						numbers[i] > 1 ? "'s" : "");
 			}
 		}
 		input.close();
