b6b34a33542081da4c968aea6eab578d48b37488
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-12 17:03:44 +0200

    Fix. isPrime() must return int not boolean. (odd)

--- Bild-IT-Zadaci/zadaci_12_08_2016/Zadatak_03.java
+++ Bild-IT-Zadaci/zadaci_12_08_2016/Zadatak_03.java
@@ -8,24 +8,24 @@ public class Zadatak_03 {
 	 * @see <a href="https://www.mkyong.com/java/how-to-determine-a-prime-number-in-java/">How to determine prime number</a>
 	 */
 	//checks whether an int is prime or not.
-	public static boolean isPrime(int n) {
+	public static int isPrime(int n) {
 	    //check if n is a multiple of 2
-	    if (n%2==0) return false;
+	    if (n%2==0 && n != 2) return 0;
 	    //if not, then just check the odds
 	    for(int i=3;i*i<=n;i+=2) {
 	        if(n%i==0)
-	            return false;
+	            return 0;
 	    }
-	    return true;
+	    return n;
 	}
 	/**
 	 * Program prints all primes in range 1 100000.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("All primes form 0 to 100.000:\n2 ");
-		for (int i = 3, counter = 0; i < 100000; i++) {
-			if (isPrime(i)) {
+		System.out.print("All primes form 0 to 100.000:\n");
+		for (int i = 0, counter = 0; i < 100000; i++) {
+			if (isPrime(i) != 0 && i != 1) {
 				System.out.print(i + " ");
 				counter++;
 				if (counter % 10 == 0) System.out.println();
