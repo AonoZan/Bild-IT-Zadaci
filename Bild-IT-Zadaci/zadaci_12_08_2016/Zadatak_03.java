245293451d96f2291a644e4bd5dd2da9270d4960
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-12 19:12:31 +0200

    Placed checking if number is 1 inside method.

--- Bild-IT-Zadaci/zadaci_12_08_2016/Zadatak_03.java
+++ Bild-IT-Zadaci/zadaci_12_08_2016/Zadatak_03.java
@@ -10,7 +10,7 @@ public class Zadatak_03 {
 	//checks whether an int is prime or not.
 	public static int isPrime(int n) {
 	    //check if n is a multiple of 2
-	    if (n%2==0 && n != 2) return 0;
+	    if (n%2==0 && n != 2 || n == 1) return 0;
 	    //if not, then just check the odds
 	    for(int i=3;i*i<=n;i+=2) {
 	        if(n%i==0)
@@ -25,7 +25,7 @@ public class Zadatak_03 {
 	public static void main(String[] args) {
 		System.out.print("All primes form 0 to 100.000:\n");
 		for (int i = 0, counter = 0; i < 100000; i++) {
-			if (isPrime(i) != 0 && i != 1) {
+			if (isPrime(i) != 0) {
 				System.out.print(i + " ");
 				counter++;
 				if (counter % 10 == 0) System.out.println();
