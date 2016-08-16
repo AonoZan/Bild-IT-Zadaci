f41b85526ed3985e4f402a96b12aca2f220e3179
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-16 13:36:11 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_16_08_2016/Zadatak_03.java
+++ Bild-IT-Zadaci/zadaci_16_08_2016/Zadatak_03.java
@@ -1,5 +1,8 @@
 
 package zadaci_16_08_2016;
+
+import java.util.Scanner;
+
 /**
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
@@ -13,11 +16,35 @@ public class Zadatak_03 {
 	public static boolean isValidSSN(String number) {
 		// length of number is not valid
 		if (number.length() < 11) return false;
+		// loop trough whole number
+		for (int i = 0; i < number.length(); i++) {
+			char ch = number.charAt(i);
+			if (i == 3 || i == 6) {				// if character on 4zh place or 7th place 
+				if (ch != '-') {				// isn't -
+					return false;				// SSN is not valid
+				}
+			} else if (!Character.isDigit(ch)) {// or if character is not digit
+				return false;
+			}
+		}
+		// SSN is valid
 		return true;
 	}
+	/**
+	 * Program asks for Social Security Number and checks if it's valid one.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		// prompt user for number
+		System.out.println("SSN format = DDD-DD-DDDD");
+		System.out.print("Enter SSN number: ");
+		// get number and check if it's valid
+		String ssn = input.nextLine();
+		if (!isValidSSN(ssn)) {
+			System.out.println("SSN is not valid.");
+		}
+		input.close();
 	}
-
 }
 
