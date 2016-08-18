c5bae5a094559e39274b930fcef8738e61ade3be
Acer <Acer@DESKTOP-VI3LOMA>
2016-08-18 10:50:41 +0200

    Progress.

--- Bild-IT-Zadaci/zadaci_18_08_2016/Zadatak_03.java
+++ Bild-IT-Zadaci/zadaci_18_08_2016/Zadatak_03.java
@@ -1,21 +1,84 @@
 
 package zadaci_18_08_2016;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class Zadatak_03 {
+	/**
+	 * Method checks list and recognizes pattern of 4 equal numbers
+	 * in a row, if pattern is found true is returned.
+	 * @param values
+	 * @return
+	 */
 	public static boolean isConsecutiveFour(int[] values) {
-		for (int i = 0; i < values.length - 4; i++) {
-			if (values[i] == values[i+1] &&
-					values[i] == values[i+2] &&
-					values[i] == values[i+3]) {
+		// in case argument is null
+		if (values == null) {
+			System.out.println("Can't find consecutive 4. (null args)");
+			return false;
+		}
+		// loop over numbers 
+		for (int i = 0; i < values.length - 3; i++) {
+			// if 4 in a row same return true
+			if (values[i] == values[i + 1]
+					&& values[i] == values[i + 2]
+					&& values[i] == values[i + 3]) {
 				return true;
 			}
 		}
 		return false;
 	}
+	/**
+	 * Program asks from user length of list and then fills list with user
+	 * input. If list contains consecutive fours true is printed else false
+	 * is printed on the console.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		System.out.println(isConsecutiveFour(new int[]{1,1,1,21,2,5,4,8, 8, 8, 8,7,9,7,8}));
+		Scanner input = new Scanner(System.in);
+		int listLen = 0;
+		int[] list;
+		System.out.println("Enter size of the list and numbers for the list.");
+		while (true) {
+			try {
+				// if length is not provided get it from user
+				if (listLen == 0) {
+					System.out.print("Enter list length: ");
+					listLen = input.nextInt();
+					// if user makes mistake loop again
+					if (listLen <= 0) {
+						listLen = 0;
+						continue;
+					}
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter number.");
+				input.nextLine();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.exit(0);
+			}
+			list = new int[listLen];
+			for (int i = 0; i < list.length; i++) {
+				try {
+					System.out.printf("Fill index %d:", i);
+					list[i] = input.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Please enter number.");
+					input.nextLine();
+					i--;
+				} catch (Exception e) {
+					System.exit(0);
+				}
+			}
+			break;
+		}
+		// print message
+		System.out.printf("Consecutive four found: %b",
+				isConsecutiveFour(list));
+		input.close();
 	}
 
 }
