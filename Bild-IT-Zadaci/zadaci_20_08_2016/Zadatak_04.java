924c441264a27f592103c906a586c53e83776ee3
AonoZan (Acer) <aonozan@gmail.com>
2016-08-20 20:56:22 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_20_08_2016/Zadatak_04.java
@@ -0,0 +1,17 @@
+
+package zadaci_20_08_2016;
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_04 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+class Account {
+	private int id = 0;
+	private double balance;
+	
+}
