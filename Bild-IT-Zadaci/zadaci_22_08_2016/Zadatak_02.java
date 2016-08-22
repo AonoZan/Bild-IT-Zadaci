d1d75cd201da20e92f5c8578cd2d477b7464f1db
AonoZan (Acer) <aonozan@gmail.com>
2016-08-22 19:42:13 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_22_08_2016/Zadatak_02.java
+++ Bild-IT-Zadaci/zadaci_22_08_2016/Zadatak_02.java
@@ -4,12 +4,16 @@ package zadaci_22_08_2016;
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class Zadatak_02 {
-
+	/**
+	 * Method creates three polygons and prints their perimeter and area.
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// create three polygons
 		RegularPolygon poly1 = new RegularPolygon();
 		RegularPolygon poly2 = new RegularPolygon(4, 4);
 		RegularPolygon poly3 = new RegularPolygon(10, 4, 5.6, 7.8);
-		
+		// print status
 		System.out.printf
 				( "Polygon 1"
 				+ "\n\tPerimeter: %.2f"
@@ -33,15 +37,25 @@ public class Zadatak_02 {
 				);
 	}
 }
+/**
+ * Simple polygon class that has getters, setters
+ * and methods for parameter and area.
+ * @author AonoZan
+ *
+ */
 class RegularPolygon {
+	// default values
 	private int n = 3;
 	private double side = 1;
 	private double x = 0, y = 0;
+	// no-arg constructor
 	public RegularPolygon() {
 	}
+	// that takes number of sides and length
 	public RegularPolygon(int n, double side) {
 		this(n, side, 0, 0);
 	}
+	// constructor that takes sides, size and location of the polygon
 	public RegularPolygon(int n, double side, double x, double y) {
 		this.n = n;
 		this.side = side;
@@ -72,6 +86,7 @@ class RegularPolygon {
 	public void setY(double y) {
 		this.y = y;
 	}
+	// perimeter and area methods
 	public double getPerimeter() {
 		return this.n * this.side;
 	}
