977fac249db7f8a8bcef9094d5a8ff7b8f45f238
AonoZan (Acer) <aonozan@gmail.com>
2016-08-23 09:14:31 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_22_08_2016/Zadatak_04.java
@@ -0,0 +1,48 @@
+
+package zadaci_22_08_2016;
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_04 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+class LinearEquation {
+	private double a;
+	private double b;
+	private double c;
+	private double d;
+	private double e;
+	private double f;
+	public LinearEquation(double a, double b, double c, double d, double e, double f) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+		this.e = e;
+		this.f = f;
+	}
+	public double getA() {
+		return a;
+	}
+	public double getB() {
+		return b;
+	}
+	public double getC() {
+		return c;
+	}
+	public double getD() {
+		return d;
+	}
+	public double getE() {
+		return e;
+	}
+	public double getF() {
+		return f;
+	}
+	
+}
