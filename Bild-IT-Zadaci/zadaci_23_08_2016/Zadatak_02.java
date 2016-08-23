3aa71dee96d1c9523a07ea1d5b02256c64a66c62
AonoZan (Acer) <aonozan@gmail.com>
2016-08-23 21:27:16 +0200

    Progress.

--- Bild-IT-Zadaci/zadaci_23_08_2016/Zadatak_02.java
+++ Bild-IT-Zadaci/zadaci_23_08_2016/Zadatak_02.java
@@ -6,17 +6,51 @@ package zadaci_23_08_2016;
 public class Zadatak_02 {
 
 	public static void main(String[] args) {
-
+		MyInteger integer = new MyInteger(4);
+		System.out.println(integer.isPrime());
 	}
 
 }
 class MyInteger {
 	private int value;
-	
+	public MyInteger(int value) {
+		super();
+		this.value = value;
+	}
 	public boolean isEven() {
-		return this.value % 2 == 0 ? true : false;
+		return isEven(this.value);
+	}
+	public static boolean isEven(int n) {
+		return n % 2 == 0 ? true : false;
+	}
+	public static boolean isEven(MyInteger number) {
+		return isEven(number.value);
 	}
 	public boolean isOdd() {
 		return !isEven();
+	}
+	public static boolean isOdd(int n) {
+		return !isEven(n);
+	}
+	public static boolean isOdd(MyInteger number) {
+		return !isEven(number.value);
+	}
+	public boolean isPrime() {
+		return isPrime(this.value);
+	}
+	public static boolean isPrime(int n) {
+		// check if number is only 
+		if (n % 2 == 0 && n != 2 || n <= 1) {
+			return false;
+		}
+		//if not, then just check the odds
+		for (int i = 3; i * i <= n; i += 2) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
+	public static boolean isPrime(MyInteger number) {
+		return isPrime(number.value);
 	}
 }
