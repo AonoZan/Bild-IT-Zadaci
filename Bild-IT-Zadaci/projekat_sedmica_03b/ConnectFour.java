789e7f5d3a28975ffe53c47ba3ec243f3ab6faec
AonoZan (Acer) <aonozan@gmail.com>
2016-08-23 17:47:33 +0200

    Finish.

--- Bild-IT-Zadaci/projekat_sedmica_03b/ConnectFour.java
+++ Bild-IT-Zadaci/projekat_sedmica_03b/ConnectFour.java
@@ -4,25 +4,35 @@ package projekat_sedmica_03b;
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class ConnectFour {
-	private String[][] table;
+	//create table and store table width and height
+	private char[][] table;
+	private int tW, tH;
+	private int maxTurn, turn = 0;
 	// constructor that creates table for desired rows and columns
 	public ConnectFour(int rows, int columns) {
-		this.table = new String[rows][columns];
+		this.tH = rows - 1;
+		this.tW = columns - 1;
+		this.maxTurn = rows * columns;
+		this.table = new char[rows][columns];
 	}
+	/**
+	 * Method for printing table on console.
+	 */
 	public void printTable() {
-		System.out.println("");
-		for (String[] strings : table) {
+		System.out.println("_______________");
+		for (char[] chars : table) {
 			System.out.print("|");
-			for (String string : strings) {
-				if (string == null) {
+			for (char ch : chars) {
+				if (ch == '\u0000') {
 					System.out.print(" ");
 				} else {
-					System.out.print(string);
+					System.out.print(ch);
 				}
 				System.out.print("|");
 			}
 			System.out.println();
 		}
+		System.out.println("'''''''''''''''");
 	}
 	/**
 	 * Make move by placing player on available place on table.
@@ -30,17 +40,78 @@ public class ConnectFour {
 	 * @param column
 	 * @return
 	 */
-	public boolean makeMove(String player, int column) {
+	public boolean makeMove(char player, int column) {
 		// loop for every row
-		for (int i = table.length - 1; i <= 0; i++) {
+		for (int i = table.length - 1; i >= 0; i--) {
 			// on first one available place player
-			if (table[i][column] == null) {
+			if (table[i][column] == '\u0000') {
 				table[i][column] = player;
+				this.turn++;
 				return true;
 			}
 		}
 		// player wasn't placed
 		return false;
+	}
+	public char winner() {
+		// loop for all horizontally places
+		// search pattern for 7x5 table length 4
+		// vertical horisontal and diagonal
+		// ±±±±±±±  ±±±±+++  ±±±±        ±±±±
+		// ±±±±±±±  ±±±±+++  ±±±±+      +±±±±
+		// ±±±±±±±  ±±±±+++  ±±±±++    ++±±±±
+		// +++++++  ±±±±+++   ++++++  ++++++
+		// +++++++  ±±±±+++    +++++  +++++
+		// +++++++  ±±±±+++     ++++  ++++
+		for (int i = 0; i <= this.tH; i++) {
+			for (int j = 0; j <= this.tW - 3; j++) {
+				// if four places horizontally are equal return player
+				if (table[i][j] != '\u0000'
+						&& table[i][j] == table[i][j+1]
+						&& table[i][j] == table[i][j+2]
+						&& table[i][j] == table[i][j+3]) {
+					return table[i][j];
+				}
+			}
+		}
+		// check for vertical places
+		for (int i = 0; i <= this.tW; i++) {
+			for (int j = 0; j <= this.tH - 3; j++) {
+				// if all four places verticaly are equal return player
+				if (table[j][i] != '\u0000'
+						&& table[j][i] == table[j+1][i]
+						&& table[j][i] == table[j+2][i]
+						&& table[j][i] == table[j+3][i]) {
+					return table[j][i];
+				}
+			}
+		}
+		for (int i = 0; i <= this.tH - 3; i++) {
+			for (int j = 0; j <= this.tW - 3; j++) {
+				// return player on all four places diagonal(up left to down right)
+				if (table[i][j] != '\u0000'
+						&& table[i][j] == table[i+1][j+1]
+						&& table[i][j] == table[i+2][j+2]
+						&& table[i][j] == table[i+3][j+3]) {
+					return table[i][j];
+				}
+			}
+		}
+		for (int i = 0; i <= this.tH - 3; i++) {
+			for (int j = this.tW; j >= 3; j--) {
+				// return player on all four places (up right to down left)
+				if (table[i][j] != '\u0000'
+						&& table[i][j] == table[i+1][j-1]
+						&& table[i][j] == table[i+2][j-2]
+						&& table[i][j] == table[i+3][j-3]) {
+					return table[i][j];
+				}
+			}
+		}
+		if (this.turn == this.maxTurn) {
+			return 'X';
+		}
+		return '\u0000';
 	}
 }
 
--- Bild-IT-Zadaci/projekat_sedmica_03b/Main.java
+++ Bild-IT-Zadaci/projekat_sedmica_03b/Main.java
@@ -1,12 +1,93 @@
 
 package projekat_sedmica_03b;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class Main {
-
+	
+	static Scanner input;
+	/**
+	 * Method for clearing console and waiting until user makes action.
+	 */
+	public static void waitResponse(){
+		System.out.println("Press enter...");
+		input.nextLine();
+		input.nextLine();
+	}
+	/**
+	 * Program is well known game "connect four".
+	 * Rules are that each player takes turns and board is filled
+	 * with players discs blue or red ones(B or R).
+	 * winner is player who manages to place four in a row
+	 * horizontally, vertically or diagonally.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
+		input = new Scanner(System.in);
+		// create game with 6x7 table
+		ConnectFour game = new ConnectFour(6, 7);
+		// boolean for turns
+		boolean playerTurn = true;
+		// players signatures
+		char player1 = 'B', player2 = 'R';
+		// current player and winning player
+		char player, winner = '\u0000';
+		int inputNum = 0;
+		// play until game breaks
+		do {
+			// print table
+			System.out.println("\n\n\n\n\n\n\n\n\n\n");
+			game.printTable();
+			// try to place players disc on the table
+			try {
+				// set correct player based on turn and prompt player too play
+				player = playerTurn ? player1 : player2;
+				System.out.printf("Drop a %s disk at column (0–6): ", playerTurn ? "blue" : "red");
+				inputNum = input.nextInt();
+				// if incorrect column loop again after user action
+				if (inputNum > 6 || inputNum < 0) {
+					System.out.println("Please enter value between 0 a0nd 6.");
+					waitResponse();
+					continue;
+				}
+				// if player tried to play on column that's full loop again
+				if (!game.makeMove(player, inputNum)) {
+					System.out.println("That collumn is full.");
+					waitResponse();
+					continue;
+				}
+				// get winner and if it's not empty break of the game
+				winner = game.winner();
+				if (winner != '\u0000') {
+					break;
+				}
+				// switch turn for next player
+				playerTurn = !playerTurn;
+				
+			} catch (InputMismatchException e){
+				System.out.println("Please enter only numbers.");
+				input.nextLine();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.exit(0);
+			}
+			
+		} while (true);
+		// print table after game finishes
+		System.out.println("\n\n\n\n\n\n\n\n\n\n");
+		game.printTable();
+		// and print status of who won the game
+		System.out.println("Game over.");
+		if (winner == 'X') {
+			System.out.println("It's a draw!");
+		} else {
+			System.out.printf("Winner is %s player!\n", playerTurn ? "blue" : "red");
+		}
+		input.close();
 	}
 
 }
