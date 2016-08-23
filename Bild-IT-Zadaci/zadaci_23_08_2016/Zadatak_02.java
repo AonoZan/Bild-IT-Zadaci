9b909a6f98db0c71e1bae3a1dbf6dc3a51c5f05a
AonoZan (Acer) <aonozan@gmail.com>
2016-08-23 21:54:04 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_23_08_2016/Zadatak_02.java
+++ Bild-IT-Zadaci/zadaci_23_08_2016/Zadatak_02.java
@@ -4,19 +4,51 @@ package zadaci_23_08_2016;
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class Zadatak_02 {
-
+	/**
+	 * Test method for class MyInteer
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		MyInteger integer = new MyInteger(4);
-		System.out.println(integer.isPrime());
+		// create one integer and do some benchmarking
+		MyInteger integer = new MyInteger(13);
+		System.out.printf("Number %d is even: %b"
+				, integer.getValue()
+				, integer.isEven());
+		System.out.printf("Number %d is odd: %b"
+				, integer.getValue()
+				, integer.isOdd());
+		System.out.printf("Number %d is prime: %b"
+				, integer.getValue()
+				, integer.isPrime());
+		System.out.printf("Number %d is even: %b"
+				, 54
+				, MyInteger.isEven(54));
+		System.out.printf("Number %d is odd: %b"
+				, 54
+				, MyInteger.isOdd(54));
+		System.out.printf("Number %d is prime: %b"
+				, 54
+				, MyInteger.isPrime(54));
+		System.out.println("String 43780 to integer: "
+				+ MyInteger.parseInt("43780"));
 	}
-
 }
+/**
+ * Simple class that stores one integer.
+ * Class has range of useful methods that are convenient.
+ * @author AonoZan
+ *
+ */
 class MyInteger {
 	private int value;
 	public MyInteger(int value) {
 		super();
 		this.value = value;
 	}
+	public int getValue() {
+		return this.value;
+	}
+	// methods for checking if some numbers are even
 	public boolean isEven() {
 		return isEven(this.value);
 	}
@@ -26,6 +58,7 @@ class MyInteger {
 	public static boolean isEven(MyInteger number) {
 		return isEven(number.value);
 	}
+	// for checking if odd
 	public boolean isOdd() {
 		return !isEven();
 	}
@@ -35,6 +68,7 @@ class MyInteger {
 	public static boolean isOdd(MyInteger number) {
 		return !isEven(number.value);
 	}
+	// for checking if prime or not
 	public boolean isPrime() {
 		return isPrime(this.value);
 	}
@@ -52,5 +86,40 @@ class MyInteger {
 	}
 	public static boolean isPrime(MyInteger number) {
 		return isPrime(number.value);
+	}
+	// methods for checking if some number is equal to this class number
+	public boolean equals(int n) {
+		return this.value == n ? true : false;
+	}
+	public boolean equals(MyInteger number) {
+		return equals(number.value);
+	}
+	/**
+	 * Method for converting list of char values to integer.
+	 * @param number
+	 * @return
+	 */
+	public static int parseInt(char[] number) {
+		// create parsed number and loop over list
+		int parsed = 0;
+		for (char c : number) {
+			// if char is digit
+			if (Character.isDigit(c)) {
+				// shift number to the right
+				// and assign char to the last digit
+				parsed *= 10;
+				parsed += c - 48;
+			} else {
+				//print error massage and return 0
+				System.out.print("Illegal character in parseInt() ==> ");
+				System.out.println(c);
+				return 0;
+			}
+		}
+		return parsed;
+	}
+	// method for parsing string into number
+	public static int parseInt(String number) {
+		return parseInt(number.toCharArray());
 	}
 }
