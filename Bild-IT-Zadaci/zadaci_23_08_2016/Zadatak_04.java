78949ec6923c5d43b94023f7d840295a59838921
AonoZan (Acer) <aonozan@gmail.com>
2016-08-24 09:15:39 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_23_08_2016/Zadatak_04.java
+++ Bild-IT-Zadaci/zadaci_23_08_2016/Zadatak_04.java
@@ -11,20 +11,36 @@ public class Zadatak_04 {
 	/**Program makes list of all primes less than 120 and prints in reverse*/
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		// create list and variable to store number
 		StackOfIntegers factors = new StackOfIntegers();
 		int userNumber = 0;
+		// prompt user for number
 		System.out.println("Enter one number to be calculated factors from.");
 		do {
 			try {
+				// try to get number
 				System.out.print("Enter (non-zero) number: ");
 				userNumber =input.nextInt();
 			} catch (InputMismatchException e) {
+				// if error clear console
 				System.out.println("Please enter only numbers.");
+				input.nextLine();
 			}
 		} while (userNumber == 0);
-		
-		while (userNumber > 1)
-		// pop primes and print them
+		// create first divider and loop until number becomes non divisable
+		int divider = 2;
+		while (userNumber > 1) {
+			// if number can be divided add to list divider and divide number
+			if (userNumber % divider == 0) {
+				factors.push(divider);
+				userNumber /= divider;
+			// else try another divider
+			} else {
+				divider++;
+			}
+		}
+		// pop factors and print them
+		System.out.print("Factors: ");
 		for (int i = factors.getSize(); i > 0; i--) {
 			System.out.print(factors.pop() + " ");
 		}
