b08929df462c2ccc73704c12590d296ad81ca5f2
AonoZan (Acer) <aonozan@gmail.com>
2016-08-25 15:11:57 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_25_08_2016/Zadatak_01.java
@@ -0,0 +1,59 @@
+
+package zadaci_25_08_2016;
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_01 {
+	
+	public static void main(String[] args) {
+	}
+
+}
+
+class MyString1 {
+	private char[] value;
+	MyString1() {
+		this.value = null;
+	}
+	MyString1(String string) {
+		this(string.toCharArray());
+	}
+	MyString1(char[] chars) {
+		if (chars == null) {
+			this.value = null;
+			return;
+		}
+		this.value = chars;
+	}
+	public char charAt(int index) {
+		return this.value[index];
+	}
+	public int length() {
+		return this.value.length;
+	}
+	public MyString1 substring(int begin, int end){
+		// wrong size arguments return null
+		if ((begin + end) > length()
+				|| begin < 0
+				|| end < 0) {
+			System.out.println("can't return substring.(IndexOutOfRange)");
+			return null;
+		}
+		// switch values if end is less than begin 
+		if (begin > end) {
+			begin += end;
+			end = begin - end;
+			begin -= end;
+		}
+		char[] values = new char[end - begin];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = this.value[begin];
+			begin++;
+		}
+		return new MyString1(values);
+	}
+	public MyString1 toLowerCase();
+	public boolean equals(MyString1 s);
+	public static MyString1 valueOf(int i);
+	
+}
