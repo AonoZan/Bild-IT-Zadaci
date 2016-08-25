265c92b8eba863c6485a67f8b6792528ea34a83c
AonoZan (Acer) <aonozan@gmail.com>
2016-08-25 19:09:22 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_25_08_2016/Zadatak_04.java
@@ -0,0 +1,18 @@
+
+package zadaci_25_08_2016;
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_04 {
+	public static String[] split(String s, String regex) {
+		String[] splited = s.split(regex);
+		String[] newList = new String[splited.length * 2 - 1];
+		
+		return newList;
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
+
