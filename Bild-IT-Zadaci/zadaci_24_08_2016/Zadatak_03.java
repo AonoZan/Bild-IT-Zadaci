6db50edd164281d63461b3b5e491ec3da8783190
AonoZan (Acer) <aonozan@gmail.com>
2016-08-25 08:52:02 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_24_08_2016/Zadatak_03.java
@@ -0,0 +1,22 @@
+
+package zadaci_24_08_2016;
+
+import java.math.BigDecimal;
+
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_03 {
+	public static boolean isPrime(BigDecimal n) {
+		
+		return true;
+	}
+	/**
+	 * Program finds five prime numbers greater than long max value.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+#}
+
