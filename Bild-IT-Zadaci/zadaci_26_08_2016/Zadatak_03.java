7e0eeff725b7a773835d2ef3930761a9d2f42e92
AonoZan (Acer) <aonozan@gmail.com>
2016-08-26 16:26:43 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_26_08_2016/Zadatak_03.java
@@ -0,0 +1,20 @@
+
+package zadaci_26_08_2016;
+
+import java.util.ArrayList;
+
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_03 {
+	public static Integer max(ArrayList<Integer> list) {
+		Integer number = new Integer(0);
+		
+		return number;
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
+
