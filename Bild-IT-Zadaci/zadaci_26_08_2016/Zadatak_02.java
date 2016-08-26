3a835f77676ad7b9e41dda6861c1b2506981eda8
AonoZan (Acer) <aonozan@gmail.com>
2016-08-26 16:24:18 +0200

    Finish.

--- Bild-IT-Zadaci/zadaci_26_08_2016/Zadatak_02.java
+++ Bild-IT-Zadaci/zadaci_26_08_2016/Zadatak_02.java
@@ -7,51 +7,56 @@ import java.util.Date;
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class Zadatak_02 {
-
+	/**Program tests several classes: Person, Student, Employee, Facility and Staff*/
 	public static void main(String[] args) {
+		// create person with all attributes and print it
 		Person zdravko = new Person("Zdravko Mrèina", "Tunguziæi donji", "zm_ladysman15@mail.com", "053/789-987");
 		System.out.println("Person:\n" + zdravko.toString());
-		
+		// create student assign status and print
 		Student cvetko = new Student("Cvetko Brat", "Pribiniæi", "bukva_i_rast789@hotmail.com", "+38766/787-887");
 		cvetko.setStatus(Student.CLASS_0);
 		System.out.println("Student:\n" + cvetko.toString());
-		
+		// create employee set office and salary
 		Employee boris = new Employee("Boris Vajiæ", "Derventa", "boris_vajic331@gmail.com", "066785453");
 		boris.setOffice("Derventa");
 		boris.setSalary(840);
 		System.out.println("Employee:\n" + boris.toString());
-		
+		// create facility, set office, salary, hours and rank
 		Facility ratko = new Facility("Ratko Bošnjak", "Maglaj", "ratkoratkoko14@gmail.com", "064-789-654");
 		ratko.setOffice("Doboj");
 		ratko.setSalary(1200);
 		ratko.setOfficeHours(8);
 		ratko.setRank("šef stanice");
 		System.out.println("Facility:\n" + ratko.toString());
-		
-		Facility slavica = new Facility("Slavica Kokuziæ", "Pridjel", "slavica_slava96@gmail.com", "066/365-065");
-		slavica.setOffice("Bivši modeks");
-		slavica.setSalary(600);
-		slavica.setOfficeHours(12);
-		slavica.setRank("Rukovodilac šivaæih mašinama");
+		// create staff add office, salary and title
+		Staff slavica = new Staff("Slavica Kokuziæ", "Pridjel", "slavica_slava96@gmail.com", "066/365-065");
+		slavica.setOffice("Art studio Galebra");
+		slavica.setSalary(2500);
+		slavica.setTitle("Muza");
 		System.out.println("Staff:\n" + slavica.toString());
 	}
 }
-
+/**Simple person class.*/
 class Person {
-	private String name, adress, email, phone;
+	// create attributes
+	private String name, address, email, phone;
+	// default person has name set to N/A
 	public Person() {
 		this("N/A");
 	}
+	// Person with name has phone set to N/A
 	public Person(String name) {
 		this(name, "N/A");
 	}
+	// Person with name and phone has email and address set to N/A
 	public Person(String name, String phone) {
 		this(name, "N/A", "N/A", phone);
 	}
+	// create person with all atributes
 	public Person(String name, String adress, String email, String phone) {
 		super();
 		this.name = name;
-		this.adress = adress;
+		this.address = adress;
 		this.email = email;
 		this.phone = phone;
 	}
@@ -62,11 +67,11 @@ class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getAdress() {
-		return adress;
+	public String getAddress() {
+		return address;
 	}
 	public void setAdress(String adress) {
-		this.adress = adress;
+		this.address = adress;
 	}
 	public String getEmail() {
 		return email;
@@ -90,18 +95,21 @@ class Person {
 				, this.name
 				, this.email
 				, this.phone
-				, this.adress
+				, this.address
 				);
 	}
 }
+/**Simple student class.*/
 class Student extends Person {
+	// student class have classes
 	final static String CLASS_0 = "Freshman";
 	final static String CLASS_1 = "Sophomore";
 	final static String CLASS_2 = "Junior";
 	final static String CLASS_3 = "Senior";
-	
+	// variable to store class as status
 	private String status;
-	
+	// constructors that call super constructor to create person
+	// other attributes can be set with mutators
 	public Student() {
 		super();
 	}
@@ -131,10 +139,13 @@ class Student extends Person {
 	}
 }
 class Employee extends Person {
+	// employee have office and salary
 	private String office;
 	private int salary;
+	// date when employee is hired is created automatically
 	private Date dateHired = new Date();;
-	
+	// constructors that call super constructor to create person
+	// other attributes can be set with mutators
 	public Employee() {
 		super();
 	}
@@ -147,6 +158,7 @@ class Employee extends Person {
 	public Employee(String name, String addres, String email, String phone) {
 		super(name, addres, email, phone);
 	}
+	// accessors and mutators
 	public String getOffice() {
 		return office;
 	}
@@ -173,10 +185,13 @@ class Employee extends Person {
 				);
 	}
 }
+/**Simple facility class.*/
 class Facility extends Employee {
+	// facility have office hours and rank
 	private int officeHours;
 	private String rank;
-	
+	// constructors that call super constructor to create person
+	// other attributes can be set with mutators
 	public Facility() {
 		super();
 	}
@@ -189,6 +204,7 @@ class Facility extends Employee {
 	public Facility(String name, String addres, String email, String phone) {
 		super(name, addres, email, phone);
 	}
+	// accessors and mutators
 	public int getOfficeHours() {
 		return officeHours;
 	}
@@ -210,6 +226,41 @@ class Facility extends Employee {
 				, super.toString()
 				, this.officeHours
 				, this.rank
+				);
+	}
+}
+/**Simple staff class.*/
+class Staff extends Employee {
+	// facility have title
+	private String title;
+	// constructors that call super constructor to create person
+	// other attributes can be set with mutators
+	public Staff() {
+		super();
+	}
+	public Staff(String name) {
+		super(name);
+	}
+	public Staff(String name, String phone) {
+		super(name, phone);
+	}
+	public Staff(String name, String addres, String email, String phone) {
+		super(name, addres, email, phone);
+	}
+	// accessors and mutators
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	@Override
+	public String toString() {
+		return String.format
+				( "%s"
+				+ "\tTitle: %s\n"
+				, super.toString()
+				, this.title
 				);
 	}
 }
