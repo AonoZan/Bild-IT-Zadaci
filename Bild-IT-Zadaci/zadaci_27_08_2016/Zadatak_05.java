1986212536fbc1a66c158d746bee69a35e497c6c
AonoZan (Acer) <aonozan@gmail.com>
2016-08-27 16:31:38 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_27_08_2016/Zadatak_05.java
@@ -0,0 +1,20 @@
+
+package zadaci_27_08_2016;
+
+import java.util.ArrayList;
+
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_05 {
+	public static ArrayList<Integer> union(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+		ArrayList<Integer> union = new ArrayList<>();
+		
+		return union;
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
+
