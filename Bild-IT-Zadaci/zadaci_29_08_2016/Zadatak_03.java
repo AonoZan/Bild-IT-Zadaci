0192cf4cf749c634c4cd17ac8a08bfeafd11f46f
AonoZan (Acer) <aonozan@gmail.com>
2016-08-30 15:32:05 +0200

    Comments.

--- Bild-IT-Zadaci/zadaci_29_08_2016/Zadatak_03.java
+++ Bild-IT-Zadaci/zadaci_29_08_2016/Zadatak_03.java
@@ -1,7 +1,6 @@
 
 package zadaci_29_08_2016;
 
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -12,14 +11,19 @@ public class Zadatak_03 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		// create two random numbers
 		int randNum1 = (int)(Math.random() * 100);
 		int randNum2 = (int)(Math.random() * 100);
+		// create variable for sum and boolean for loop
 		int sum;
 		boolean needAnswer = true;
+		// loop as long as answer is needed
 		while (needAnswer) {
 			try {
+				// ask for answer
 				System.out.printf("Enter sum of %d + %d: ", randNum1, randNum2);
 				sum = input.nextInt();
+				// if correct answer congrats else ask again
 				if (sum == (randNum1 + randNum2)) {
 					System.out.println("\nThats correct answer.");
 					needAnswer = false;
@@ -28,6 +32,7 @@ public class Zadatak_03 {
 					System.out.println("\nThat's not correct...\n");
 				}
 			} catch (InputMismatchException e) {
+				// for wrong input print message and clear console
 				System.out.println("Please enter integer.");
 				input.nextLine();
 			}
