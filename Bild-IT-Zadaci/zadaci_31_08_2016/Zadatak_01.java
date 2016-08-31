ede50ac4b42a8a68a6a8be7dc90a772d815fb3ec
AonoZan (Acer) <aonozan@gmail.com>
2016-08-31 21:32:50 +0200

    Done before...

---
+++ Bild-IT-Zadaci/zadaci_31_08_2016/Zadatak_01.java
@@ -0,0 +1,37 @@
+
+package zadaci_31_08_2016;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_01 {
+	/** Method reformats java source code. */
+	public static String formatSC(String sourceCode) {
+		// convert from next-line style to end-line style format
+		sourceCode = sourceCode.replace("\n{", " {");
+		sourceCode = sourceCode.replace("\n\t{", " {");
+		sourceCode = sourceCode.replace("\n {", " {");
+		sourceCode = sourceCode.replace("\n  {", " {");
+		return sourceCode;
+	}
+	/** 
+	 * Program converts next line style java source code to end line style.
+	 * Source code filename is taken from parameters.
+	 */
+	public static void main (String[] args) throws IOException {
+		// get filename from parameters and create file
+        File file = new File(args[0]);
+        Scanner fileInput = new Scanner(file);
+        // read whole file and print formated
+		String content = fileInput.useDelimiter("\\Z").next();
+		System.out.println(formatSC(content));
+		
+		fileInput.close();
+    }
+
+}
+
