41f4da3cfc677b79b4bc17ef58a4ddf5c7d6d740
AonoZan (Acer) <aonozan@gmail.com>
2016-08-31 21:56:41 +0200

    Upload.

---
+++ Bild-IT-Zadaci/zadaci_31_08_2016/next_line_style.java
@@ -0,0 +1,9 @@
+package zadaci_31_08_2016;
+
+public class next_line_style
+{
+public static void main(String[] args)
+{
+// Some statements
+}
+}
---
+++ Bild-IT-Zadaci/zadaci_31_08_2016/reformat.cmd
@@ -0,0 +1,3 @@
+rem "C:\Program Files\Java\jdk1.8.0_77\bin\javac" Zadatak_01.java
+"C:\Program Files\Java\jdk1.8.0_77\bin\java" -cp . Zadatak_01 next_line_style.java
+pause
