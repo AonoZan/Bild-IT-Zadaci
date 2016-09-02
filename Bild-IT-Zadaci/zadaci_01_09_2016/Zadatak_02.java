cf23d7a06d0d9613a783640fc6270c94ace143e9
AonoZan (Acer) <aonozan@gmail.com>
2016-09-02 15:45:16 +0200

    Upload.

--- Bild-IT-Zadaci/zadaci_01_09_2016/Zadatak_02.java
+++ Bild-IT-Zadaci/zadaci_01_09_2016/Zadatak_02.java
@@ -9,26 +9,36 @@ import java.util.Scanner;
  *  @author AonoZan Dejan Petrovic 2016 ©
  */
 public class Zadatak_02 {
+	/** Method prints occurrences of each letter in a string */
 	public static void printOccurences(String content) {
-		int[] chars = new int[128];
+		// constant for limit (ignore every char above this)
+		final int MAX_CHAR = 128;
+		// create list of chars
+		int[] chars = new int[MAX_CHAR];
+		// go trough whole string and count chars
 		for (char ch : content.toCharArray()) {
-			if (ch < 128) {
+			if (ch < MAX_CHAR) {
 				chars[ch]++;
 			}
 		}
+		// print all counts (if any) for chars A to Z
 		for (int i = 'A'; i <= 'Z'; i++) {
 			if (chars[i] > 0) {
-				System.out.printf("Character \'%c\' have occured %d time%s.\n", (char) (i), chars[i],
-						chars[i] > 1 ? "'s" : "");
+				System.out.printf("Character \'%c\' have occured %d time%s.\n"
+						, (char) (i), chars[i]
+						, chars[i] > 1 ? "'s" : "");
 			}
 		}
+		// print all counts (if any) for chars a to z
 		for (int i = 'a'; i <= 'z'; i++) {
 			if (chars[i] > 0) {
-				System.out.printf("Character \'%c\' have occured %d time%s.\n", (char) (i), chars[i],
-						chars[i] > 1 ? "'s" : "");
+				System.out.printf("Character \'%c\' have occured %d time%s.\n"
+						, (char) (i), chars[i]
+						, chars[i] > 1 ? "'s" : "");
 			}
 		}
 	}
+	/** Program asks from user file path and counts each character occurrences. */
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner input = new Scanner(System.in);
 		// initialize variables with temporary values
