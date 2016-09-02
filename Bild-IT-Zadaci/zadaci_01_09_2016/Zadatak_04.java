a6438abe9438d8f341c008bae8431bde2b00dd44
AonoZan (Acer) <aonozan@gmail.com>
2016-09-02 17:23:55 +0200

    Finish.

---
+++ Bild-IT-Zadaci/zadaci_01_09_2016/Zadatak_04.java
@@ -0,0 +1,52 @@
+
+package zadaci_01_09_2016;
+
+import java.util.ArrayList;
+
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_04 {
+	/** Method returns random integer in range.*/
+	public static int randInt(int from, int to) {
+		return from + (int)(Math.random() * (to - from));
+	}
+	/** Method fills array with random values in range */
+	public static void genRand(ArrayList<Number> list, int amount, int from, int to) {
+		// loop desired number times and add random number
+		for (int i = 0; i < amount; i++) {
+			list.add(randInt(0, 10));
+		}
+	}
+	/** Method shuffles list. */
+	public static void shuffle(ArrayList<Number> list) {
+		// loop over size of the list
+		for (int i = 0; i < list.size(); i++) {
+			// generate random index and switch values
+			int randIndex = randInt(0, list.size());
+			Number tmp = list.get(i);
+			list.set(i, list.get(randIndex));
+			list.set(randIndex, tmp);
+		}
+	}
+	// print list
+	public static void printList(ArrayList<Number> list) {
+		for (Number number : list) {
+			System.out.print(number + " ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		ArrayList<Number> list = new ArrayList<>();
+		// generate list and print it
+		System.out.print("Random list: ");
+		genRand(list, 10, 0, 10);
+		printList(list);
+		// shuffle and print again
+		System.out.print("Shuffled: ");
+		shuffle(list);
+		printList(list);
+	}
+
+}
+
