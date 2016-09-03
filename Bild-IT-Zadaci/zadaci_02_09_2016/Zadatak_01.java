7f1b01c2d83094496b49c7718f748d7eef14ac5a
AonoZan (Acer) <aonozan@gmail.com>
2016-09-03 09:22:08 +0200

    Fix. Should take one argument as well...

--- Bild-IT-Zadaci/zadaci_02_09_2016/Zadatak_01.java
+++ Bild-IT-Zadaci/zadaci_02_09_2016/Zadatak_01.java
@@ -1,6 +1,10 @@
 
 package zadaci_02_09_2016;
 
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.util.Calendar;
+
 import projekat_sedmica_01.Month;
 
 /**
@@ -11,14 +15,35 @@ public class Zadatak_01 {
 	public static void main(String[] args) {
 		// create variables and try to process values from arguments
 		int year = 0, month = 0;
-		if (args.length == 2) {// there should be two arguments
-			try {
-				year = Integer.parseInt(args[0]);	// process year as integer
-				month = Integer.parseInt(args[1]);	// process month
-			} catch (Exception e) {
-				// wrong arguments exit
-				System.out.println("Error in parsing arguments. (year month)");
-				System.exit(0);
+		if (args.length > 0) {// there should be two arguments
+			if (args.length > 1) { // if more than two arguments
+				try {
+					year = Integer.parseInt(args[0]);	// process year as integer
+					month = Integer.parseInt(args[1]);	// process month
+				} catch (Exception e) {
+					// wrong arguments exit
+					System.out.println("Error in parsing arguments. (year month)");
+					System.exit(0);
+				}
+			} else { // if only one argument
+				// create loval time
+				LocalDateTime timePoint = LocalDateTime.now();
+				try {
+					// try to parse argument
+					int argument = Integer.parseInt(args[0]);
+					// if argument is potential month set it so
+					if (argument >= 1 && argument <= 12) {
+						month = argument;
+						year = timePoint.getYear();
+					} else { // else set it to year
+						month = timePoint.getMonthValue();
+						year = argument;
+					}
+				} catch (Exception e) {
+					// argument couldn't be parsed, exit
+					System.out.println("Error when parsing argument.");
+					System.exit(0);
+				}
 			}
 		} else {
 			// wrong number of arguments exit
