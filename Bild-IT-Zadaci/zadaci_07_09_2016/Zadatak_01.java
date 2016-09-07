07330553daadd8aa91650226878e5e4a53f152a5
AonoZan (Acer) <aonozan@gmail.com>
2016-09-07 12:11:19 +0200

    Finish.

---
+++ Bild-IT-Zadaci/zadaci_07_09_2016/Zadatak_01.java
@@ -0,0 +1,51 @@
+
+package zadaci_07_09_2016;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *  @author AonoZan Dejan Petrovic 2016 ©
+ */
+public class Zadatak_01 {
+	// constant for division
+	public final static int TEN = 10;
+	
+	public static void intReverse(int i) {
+		if (i == 0) return;
+		else {
+			System.out.print(i % TEN);
+			intReverse(i / TEN);
+		}
+	}
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// create number and prompt user for value
+		int n = 0;
+		System.out.println("Enter one positive number.");
+		while (true) {
+			try {
+				// if number on default ask for value
+				if (n == 0) {
+					System.out.print("Number: ");
+					n = input.nextInt();
+					// if still on default ask again
+					if (n < 0) {
+						n = 0;
+						continue;
+					}
+				}
+				break;
+			} catch (InputMismatchException e) {
+				// when user makes mistake print message and clear console
+				System.out.println("Please enter number.");
+				input.nextLine();
+			}
+		}
+		// print number in reverse
+		intReverse(n);
+		
+		input.close();
+	}
+}
+
